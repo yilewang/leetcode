8230fb9ff3e252f69b8e3e4a0a250b7834142b1e
Yile Wang <yxw190015@utdallas.edu>
2021-04-24 21:19:49 -0500

    D6

---
+++ Python/D6-easy415.py
@@ -0,0 +1,10 @@
+
+
+def add_string(x, y):
+    intx = int(x)
+    inty = int(y)
+    int_result = int(intx + inty)
+    result = str(int_result)
+    return str(result)
+
+print(add_string("1", "2"))
