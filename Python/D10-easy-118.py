e78e38b88ab0bd948b70eaa69bee5e97d3ffcf2e
Wayne <yile.wang@utdallas.edu>
2021-05-19 22:45:05 -0500

    D10

---
+++ Python/D10-easy-118.py
@@ -0,0 +1,27 @@
+### leetcode D10
+
+
+def generate(numRows):
+    if numRows == 1:
+        grp = [[1]]
+    else: 
+        grp = [[0]]*numRows
+        grp[0] = [1]
+        grp[1] = [1,1]
+        for i in range(2,numRows, 1):
+            count = len(grp[i-1])
+            space = []
+            for cc in range(len(grp[i-1])):
+                try:
+                    nu = grp[i-1][cc] + grp[i-1][cc+1]
+                except:
+                    break
+                space.append(nu)
+            grp[i] = space
+            grp[i].insert(0, 1)
+            grp[i].append(1)
+
+    return grp
+
+
+print(generate(2))
