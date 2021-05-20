44cc175ad29740929512a341081ba5df18c8ef6a
Wayne <yile.wang@utdallas.edu>
2021-05-19 22:49:53 -0500

    D10-v2

--- Python/D10-easy-118.py
+++ Python/D10-easy-118.py
@@ -2,26 +2,24 @@
 
 
 def generate(numRows):
-    if numRows == 1:
-        grp = [[1]]
-    else: 
-        grp = [[0]]*numRows
+        grp = [[1]]*numRows
         grp[0] = [1]
-        grp[1] = [1,1]
-        for i in range(2,numRows, 1):
-            count = len(grp[i-1])
-            space = []
-            for cc in range(len(grp[i-1])):
-                try:
-                    nu = grp[i-1][cc] + grp[i-1][cc+1]
-                except:
-                    break
-                space.append(nu)
-            grp[i] = space
-            grp[i].insert(0, 1)
-            grp[i].append(1)
+        if numRows > 1:
+            grp[1] = [1,1]
+            for i in range(2,numRows, 1):
+                count = len(grp[i-1])
+                space = []
+                for cc in range(len(grp[i-1])):
+                    try:
+                        nu = grp[i-1][cc] + grp[i-1][cc+1]
+                    except:
+                        break
+                    space.append(nu)
+                grp[i] = space
+                grp[i].insert(0, 1)
+                grp[i].append(1)
 
-    return grp
+        return grp
 
 
-print(generate(2))
+print(generate(1))
